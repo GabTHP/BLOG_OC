0e3b0477329247c52bb1cb0d909cead31969582e
Gabriel Bouakira <gb@ipsip.eu>
2021-06-25 09:20:22 +0200

    setting routes

--- public/index.php
+++ public/index.php
@@ -4,8 +4,23 @@
 
 require '../vendor/autoload.php';
 
-echo 'Hello world!' ;
+require '../app/controllers/controller.php';
 
-echo $_SERVER["REQUEST_URI"];
+$uri= str_replace("/Blog_Oc/", "", $_SERVER["REQUEST_URI"]);
 
+switch ($uri) {
+
+case "":
+home();
+break;
+
+case "contact":
+contact();
+break;
+
+default :
+error404();
+break;
+
+}
 ?>
