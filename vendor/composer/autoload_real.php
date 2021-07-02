c0a40ca15e441485ff3d7fb763f3979deecbc974
Gabriel Bouakira <gb@ipsip.eu>
2021-07-02 11:31:16 +0200

    fake filling the db done

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit497e287a8e8cb4f18c8bd20340a78e19
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit497e287a8e8cb4f18c8bd20340a78e19', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit497e287a8e8cb4f18c8bd20340a78e19', 'loadClassLoader'));
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -6,7 +6,7 @@
     'aliases' => 
     array (
     ),
-    'reference' => '4c2842d37445d52128aceb3735ac4f0520c6587e',
+    'reference' => '159e103b887a9076ddfbe7bae31a970894b178ff',
     'name' => '__root__',
   ),
   'versions' => 
@@ -18,7 +18,16 @@
       'aliases' => 
       array (
       ),
-      'reference' => '4c2842d37445d52128aceb3735ac4f0520c6587e',
+      'reference' => '159e103b887a9076ddfbe7bae31a970894b178ff',
+    ),
+    'fzaninotto/faker' => 
+    array (
+      'pretty_version' => 'v1.9.2',
+      'version' => '1.9.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '848d8125239d7dbf8ab25cb7f054f1a630e68c2e',
     ),
   ),
 );
