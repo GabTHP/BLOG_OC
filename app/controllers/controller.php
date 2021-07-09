6109ea3e7e400a40399acd3f77cc87818fd5201e
Gabriel Bouakira <gb@ipsip.eu>
2021-07-09 10:35:54 +0200

    add view home

--- app/controllers/controller.php
+++ app/controllers/controller.php
@@ -2,12 +2,23 @@
 
 
 
-function contact()
+function home()
 {
-    echo "Contact!" ;
+    require '../app/views/default.layout.view.php';
+    require '../app/views/index.view.php';
 }
 
 function error404()
 {
     echo "page non trouvé" ;
+}
+
+function blog_all()
+{
+   echo 'ici tous les articles du blogs seront' ;
+}
+
+function blog_single()
+{
+    echo 'ici un seul article de blg sera';
 }
