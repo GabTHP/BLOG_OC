8b9503d79d79a1b6ddcdb91f57e7bf822db5d9ef
Gabriel Bouakira <gb@ipsip.eu>
2021-07-22 11:42:23 +0200

    get one post function

--- app/models/model.php
+++ app/models/model.php
@@ -1,18 +1,26 @@
 <?php
 
-
+require '../app/db/connDb.php';
 
     // On récupère tout le contenu de la table posts
     
    
 
-    function get_all_posts() {
-        require '../app/db/connDb.php';
-        $all_posts = $pdo->query('SELECT * FROM posts');
+function get_all_posts() {
 
-        return ($all_posts->fetchAll());
-    }
+$all_posts = $pdo->query('SELECT * FROM posts');
 
+return ($all_posts->fetchAll());
+}
+
+function get_one_posts() {
+$one_post = $pdo->query('SELECT * FROM posts WHERE id = ? ');
+return ($one_post->fetchAll());
+}
+    
+    // On récupère le contenu d'un post
+
+    
   
 
 
